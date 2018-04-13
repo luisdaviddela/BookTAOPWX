66ffd28984826c7113627d864117226df8babe7a
Luis David De la cruz <delacruz_luis@outlook.es>
2018-04-13 00:46:30 -0500

    Creación de vista con código C#

---
+++ MyXamarinFormsApp/MyXamarinFormsApp/MyXamarinFormsApp/Pages/CP_MyCSharpView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace MyXamarinFormsApp
+{
+    public class CP_MyCSharpView : ContentPage
+    {
+        public CP_MyCSharpView()
+        {
+            Content = new StackLayout()
+            {
+
+            };
+        }
+    }
+}
