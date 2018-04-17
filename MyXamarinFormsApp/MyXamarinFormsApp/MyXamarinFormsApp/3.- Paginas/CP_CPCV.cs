597fe6e1f9b96f6bc990b7c5f6acc9ff12958216
Luis David De la cruz <delacruz_luis@outlook.es>
2018-04-16 23:57:42 -0500

    Contents/ ContentView & ContentPage

---
+++ MyXamarinFormsApp/MyXamarinFormsApp/MyXamarinFormsApp/3.- Paginas/CP_CPCV.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MyXamarinFormsApp
+{
+	public class CP_CPCV : ContentPage
+	{
+        CV_ContentView cv = new CV_ContentView();
+        public CP_CPCV ()
+		{
+            
+			Content = new StackLayout {
+				Children = {
+				}
+			};
+		}
+	}
+}
