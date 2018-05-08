f2ceec9a14d0e17b082c6cb833b02f21d64cf1a2
Luis David De la cruz <delacruz_luis@outlook.es>
2018-05-07 21:46:40 -0500

    6.- SQLite / Almacenamiento local -> Dependencia de servicio que crea la base de datos para cada plataforma
    6.- DependecyService that makes the database for each platform

---
+++ MyXamarinFormsApp/MyXamarinFormsApp/MyXamarinFormsApp.Android/SQLiteDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using MyXamarinFormsApp.Droid;
+using SQLite;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDB))]
+namespace MyXamarinFormsApp.Droid
+{
+    public class SQLiteDB : ISQLiteDB
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            //Se crea la Base de datos
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
