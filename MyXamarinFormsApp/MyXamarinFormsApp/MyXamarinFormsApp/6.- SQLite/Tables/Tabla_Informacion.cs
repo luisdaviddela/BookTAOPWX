1196373755ef12b278c4e82bff9b9cb8f94e6e05
Luis David De la cruz <delacruz_luis@outlook.es>
2018-05-07 23:08:04 -0500

    6.- SQLite / Almacenamiento local ->Creación de tablas SQLite

---
+++ MyXamarinFormsApp/MyXamarinFormsApp/MyXamarinFormsApp/6.- SQLite/Tables/Tabla_Informacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace MyXamarinFormsApp
+{
+    public class Tabla_Informacion
+    {
+        [PrimaryKey, AutoIncrement ,Unique]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Genero { get; set; }
+    }
+}
